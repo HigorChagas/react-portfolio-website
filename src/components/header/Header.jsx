962e2f76b91eec2ddabb9edbc2a4006fae70ab18
Higor Chagas <higornchagas@gmail.com>
2022-06-06 14:41:26 -0300

    Criando menu de redes sociais

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/img/me.png'
+
 function Header() {
   return (
     <header>
       <div className="container header__container">
-        
+        <h5>Hello I'm</h5>
+        <h1>Higor Chagas</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderSocials() {
+  return (
+    <div className='header__socials'>
+      <a href="#" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
